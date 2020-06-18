49929810e054f55a6fad39d708792269681b6ea5
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-06-18 23:34:54 +0200

    Add Item Modal
    Modal overlay for Item input added. Functioning.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+import Modal from "@material-ui/core/Modal";
+import SimpleModal from "./Components/ModalInputForm";
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import data from "./data";
@@ -10,12 +13,38 @@ import InputForm from "./Components/InputForm";
 import Settings from "./Components/Settings";
 import ItemsList from "./Components/ItemsList";
 import PageHeader from "./Components/PageHeader";
+import uniqueID from "./Components/uniqueID";
 
 export default function App() {
   const [listItem, setListItem] = useState();
   const [list, setList] = useState(data);
   console.log(list);
-  const inputNewItem = newItem => {};
+  const inputNewItem = () => {
+    const itemUniqueID = uniqueID();
+    console.log("Current itemUniqueID is ", itemUniqueID);
+    const newItemToAdd = {
+      name: "",
+      category: "",
+      id: itemUniqueID,
+      purchased: false
+    };
+    const [itemToAdd, setItemToAdd] = useState(newItemToAdd);
+    return (
+      <div>
+        <SimpleModal itemToAdd={itemToAdd} setItemToAdd={setItemToAdd} />
+      </div>
+      // <Modal
+      //   open={open}
+      //   onClose={handleClose}
+      //   aria-labelledby="simple-modal-title"
+      //   aria-describedby="simple-modal-description"
+      // >
+      //   <button type="button" onClick="handleNewItem">
+      //     Add Item
+      //   </button>
+      // </Modal>
+    );
+  };
 
   toggleItem = itemID => {};
 
@@ -31,7 +60,7 @@ export default function App() {
       <div>
         {" "}
         {/* <InputForm className="" /> */}
-        <PageHeader />
+        <PageHeader inputNewItem={inputNewItem()} />
       </div>
       <ItemsList list={list} />
 
--- src/Components/PageHeader.js
+++ src/Components/PageHeader.js
@@ -1,17 +1,42 @@
 import React, { useState, useEffect } from "react";
+//import SimpleModal from "./ModalInputForm";
 
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+//import Modal from "@material-ui/core/Modal";
+import Modal from "./ModalForm";
+
+import uniqueID from "./uniqueID";
 function PageHeader(props) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const itemUniqueID = uniqueID();
+  const newItemToAdd = {
+    name: "",
+    category: "",
+    id: itemUniqueID,
+    purchased: false
+  };
+  const [newItem, setNewItem] = useState(newItemToAdd);
+  console.log("props from pageheader.js", props);
   const clickHandler = event => {
     event.preventDefault();
-    props.inputNewItem();
+
+    //props.inputNewItem();
+    //return <SimpleModal />;
   };
   const changeHandler = () => {};
+
+  const handleOpen = () => {};
 
   return (
     <div>
       <h1 className="header new-header shopping-list">Shopping List</h1>
-
-      <button onChange={changeHandler} onClick={clickHandler}>
+      {modalOpen ? <Modal toggleModal={toggleModal} /> : null}
+      <button onChange={changeHandler} onClick={toggleModal}>
         Add an Item
       </button>
       <p />
